f512f77e296ebf1ec86299321fb75734a242e287
jcromerohdz <jcromerohdz@gmail.com>
2018-08-30 11:28:04 -0700

    Spark data frames introduction

---
+++ Spark_DataFrame/df.scala
@@ -0,0 +1,35 @@
+import org.apache.spark.sql.SparkSession
+
+val spar = SparkSession.builder().getOrCreate()
+
+val df = spark.read.option("header", "true").option("inferSchema","true")csv("CitiGroup2006_2008") //Finantial Crisis
+
+//First 5 rows
+df.head(5)
+
+for(row <- df.head(5)){
+    println(row)
+}
+
+//columns
+df.columns
+
+df.describe().show()
+
+//Select a column
+df.select("Volume").show()
+
+//Select multiple columns
+de.select($"Date", $"Close").show()
+
+
+//create new columns
+val df2 = df.withColumn("HighPlusLow", df("High")+df("Low"))
+
+df("High")
+
+df2.printSchema()
+
+df2("HighPlusLow").as("HPL")
+
+df2.select(df2("HighPlusLow").as("HPL"), df("Close")).show()
