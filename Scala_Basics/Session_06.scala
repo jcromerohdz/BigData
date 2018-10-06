1f717c56aa54ed1ccc9ce38c0d96f0d66a7beb26
Christian <jcromerohdz@gmail.com>
2018-10-05 18:09:03 -0700

    Iris csv

---
+++ Scala_Basics/Session_06.scala
@@ -0,0 +1,76 @@
+// def isEven(num:Int): Boolean = {
+//     return num%2 == 0
+// }
+// def isEven(num:Int): num%2 == 0
+// println(isEven(6))
+// println(isEven(3))
+
+def listEvens(list:List[Int]): String ={
+    for(n <- list){
+        if(n%2==0){
+            println(s"$n is even")
+        }else{
+            println(s"$n is odd")
+        }
+    }
+    return "Done"
+}
+
+val l = List(1,2,3,4,5,6,7,8)
+val l2 = List(4,3,22,55,7,8)
+listEvens(l)
+listEvens(l2)
+
+//3 7 afortunado
+
+def afortunado(list:List[Int]): Int={
+    var res=0
+    for(n <- list){
+        if(n==7){
+            res = res + 14
+        }else{
+            res = res + n
+        }
+    }
+    return res
+}
+
+val af= List(1,7,7)
+println(afortunado(af))
+
+def balance(list:List[Int]): Boolean={
+    var primera = 0
+    var segunda = 0
+
+    segunda = list.sum
+
+    for(i <- Range(0,list.length)){
+        primera = primera + list(i)
+        segunda = segunda - list(i)
+
+        if(primera == segunda){
+            return true
+        }
+    }
+    return false 
+}
+
+val bl = List(3,2,1)
+val bl2 = List(2,3,3,2)
+val bl3 = List(10,30,90)
+
+balance(bl)
+balance(bl2)
+balance(bl3)
+
+def palindromo(palabra:String):Boolean ={
+    return (palabra == palabra.reverse)
+}
+
+val palabra = "OSO"
+val palabra2 = "ANNA"
+val palabra3 = "JUAN"
+
+println(palindromo(palabra))
+println(palindromo(palabra2))
+println(palindromo(palabra3))
