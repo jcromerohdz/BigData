a27c80ec1bf28fdeab01dcbe8c0ce085002305dc
Christian <jcromerohdz@gmail.com>
2018-11-06 18:41:13 -0800

    Kmeans changes

--- Scala_Kmeans/k-means.scala
+++ Scala_Kmeans/k-means.scala
@@ -5,17 +5,17 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 
 val spark = SparkSession.builder().getOrCreate()
 
-import org.apache.spark.ml.clustering.KMMeans
+import org.apache.spark.ml.clustering.KMeans
 
 val dataset = spark.read.format("libsvm").load("sample_kmeans_data.txt")
 
 //trains the k-means model
-val kmeans = new KMMeans().setK(2).setSeed(1L)
+val kmeans = new KMeans().setK(2).setSeed(1L)
 val model = kmeans.fit(dataset)
 
 // Evaluate clustering by calculate Within Set Sum of Squared Errors.
-val WSSE = model.computeCOst(dataset)
-println(s"Within set sum of Squared Errors = $WSSSE")
+val WSSE = model.computeCost(dataset)
+println(s"Within set sum of Squared Errors = $WSSE")
 
 // Show results
 println("Cluster Centers: ")
