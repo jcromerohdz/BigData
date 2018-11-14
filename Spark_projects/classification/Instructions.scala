a096f8d9e2d7dc1b7d71a1cff6140302383320c4
Christian <jcromerohdz@gmail.com>
2018-11-13 18:11:20 -0800

    Spark prokjects

---
+++ Spark_projects/classification/Instructions.scala
@@ -0,0 +1,7 @@
+//Contenido del proyecto
+//1.- Objectivo: Comparacion del rendimiento siguientes algoritmos de machine learning
+// - SVM
+// - Decision Three
+// - Logistic Regresion
+// - Multilayer perceptron
+//Con el siguiente data set: https://archive.ics.uci.edu/ml/datasets/Bank+Marketing 
